a380937829a49b8734cb4fab4aa72c1be36d052f
kevin.kennedy <kevin.kennedy@LCA1548MYMN72>
2016-04-27 18:44:19 -0700

    Add project files.

---
+++ Schema/dbo/Tables/SchemaArrays.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[SchemaArrays] (
+    [Id] INT IDENTITY (1, 1) NOT NULL
+);
+
---
+++ Schema/dbo/Tables/SchemaReferences.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[SchemaReferences] (
+    [Id]            INT             IDENTITY (1, 1) NOT NULL,
+    [SchemaId]      INT             NOT NULL,
+    [SchemaArrayId] INT             NULL,
+    [Ordinal]       INT             NULL,
+    [Name]          NVARCHAR (256)  NULL,
+    [Identifier]    NVARCHAR (1024) NULL,
+    CONSTRAINT [PK_SchemaReferences] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+GO
+
+CREATE INDEX [IX_SchemaReferences_SchemaArrayIdOrdinal] ON [dbo].[SchemaReferences] ([SchemaArrayId] ASC, [Ordinal] ASC);
+
+GO
